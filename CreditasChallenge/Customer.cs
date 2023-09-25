6dd6aa7a97fd07925773a14491a0ef54336f231a
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-09-25 19:49:00 -0300

    test: create customer

---
+++ CreditasChallenge/Customer.cs
@@ -0,0 +1,21 @@
+﻿namespace CreditasChallenge
+{
+    public class Customer
+    {
+
+        public string Name { get; private set; }
+        public string Cpf { get; private set; }
+        public int Age { get; private set; }
+        public string Location { get; private set; }
+        public double Income { get; private set; }
+
+        public Customer(string name, string cpf, int age, string location, double income)
+        {
+            Name = name;
+            Cpf = cpf;
+            Age = age;
+            Location = location;
+            Income = income;
+        }
+    }
+}
---
+++ CreditasChallengeTests/CustomerTest.cs
@@ -0,0 +1,23 @@
+using CreditasChallenge;
+
+namespace CreditasChallengeTests
+{
+    public class CustomerTest
+    {
+        [Theory]
+        [InlineData("João", "123.456.789-10", 29, "SP", 9000)]
+        [InlineData("Marcos", "123.456.789-10", 22, "SP", 5000)]
+        [InlineData("Paula", "123.456.789-10", 16, "RJ", 3000)]
+
+        public void Test1(string name, string cpf, int age, string location, double income)
+        {
+            var customer = new Customer(name, cpf, age, location, income);
+            Assert.NotNull(customer);
+            Assert.Equal(name, customer.Name);
+            Assert.Equal(cpf, customer.Cpf);
+            Assert.Equal(age, customer.Age);
+            Assert.Equal(location, customer.Location);
+            Assert.Equal(income, customer.Income, 2);
+        }
+    }
+}
